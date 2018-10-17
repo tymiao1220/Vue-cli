5ee81b0c6184a1037b8bd62077ba3b2780a375fc
Miao <miaot2@fr-n-s087087.ncifcrf.gov>
2018-10-17 14:35:52 -0400

    setup registation

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1> Register </h1>
+    <input type="email" name="email" placeholder="email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="password" v-model="password">
+    <br>
+    <button @click='register'>Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
